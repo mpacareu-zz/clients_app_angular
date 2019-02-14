19b4415b5b574ab5cff62c98e6a220d85305ed40
Marian Capote <mcapote250390@gmail.com>
2019-02-14 18:11:03 -0300

    big pagination without overflow

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html'
 })
-export class PaginatorComponent implements OnInit {
-
+export class PaginatorComponent implements OnInit, OnChanges {
   @Input() paginator: any;
   private pages: number[];
-  constructor() { }
+  since: number;
+  until: number;
+  constructor() {}
 
   ngOnInit() {
-    console.log(this.paginator.totalPages);
-    this.pages = new Array(this.paginator.totalPages).fill(0).map((value, index) => index + 1);
-    console.log(this.pages );
+    this.updatePaginator();
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+     let updatedPaginator = changes ['paginator'];
+
+     if ( updatedPaginator.previousValue) {
+       this.updatePaginator();
+     }
+  }
+
+  private updatePaginator() {
+    if (this.paginator.totalPages > 5) {
+      this.since = Math.min(Math.max(1, this.paginator.number - 4), this.paginator.totalPages - 5);
+      this.until = Math.max (Math.min(this.paginator.totalPages, this.paginator.number + 4), 6);
+      this.pages = new Array( this.until - this.since + 1)
+      .fill(0)
+      .map((value, index) => this.since + index);
+    } else {
+      this.pages = new Array(this.paginator.totalPages)
+      .fill(0)
+      .map((value, index) => index + 1);
+    }
+  }
 }
